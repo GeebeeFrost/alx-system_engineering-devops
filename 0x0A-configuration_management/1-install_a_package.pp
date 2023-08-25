d5e404b34f4c099f4a09a81ae049e68b87ecfc81
GeebeeFrost <omgbolahan1@gmail.com>
2023-08-25 07:42:24 -0700

    Correct 1-install_a_package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,6 @@
-
 # This manifest installs the package flask from pip3.
 
-exec { 'apt-get update':
-  command => ' sudo usr/bin/apt-get update'
-}
-
-package { 'pip3':
-  ensure  => 'installed',
-  require => Exec['apt-get update']
-}
-
 package { 'flask':
-  ensure  => '2.1.0',
-  require => Package['pip3']
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
