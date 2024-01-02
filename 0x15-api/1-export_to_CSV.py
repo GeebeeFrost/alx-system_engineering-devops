dcbb386b3a43bb91f91446daef68dc92c648ddf2
GeebeeFrost <omgbolahan1@gmail.com>
2024-01-02 12:17:39 +0100

    Add Python script to gather data and export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This script returns information about an employee's TO-DO list progress
+and exports to a CSV file"""
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    emp_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    user_url = base_url + '/' + emp_id
+    todo_url = user_url + "/todos"
+
+    emp_res = requests.get(user_url)
+    emp_username = emp_res.json().get("username")
+
+    task_res = requests.get(todo_url)
+    tasks = task_res.json()
+
+    csv_tasks = []
+    for task in tasks:
+        csv_tasks.append(
+            [emp_id, emp_username, task.get("completed"), task.get("title")])
+    csv_file = emp_id + ".csv"
+
+    with open(csv_file, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(csv_tasks)
