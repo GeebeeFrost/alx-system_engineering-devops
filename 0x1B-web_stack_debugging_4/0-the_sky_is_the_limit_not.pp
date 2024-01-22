3e1c8611c0834d41a219cd2e23b7534a8de53f86
GeebeeFrost <omgbolahan1@gmail.com>
2024-01-22 10:22:00 +0000

    Add Puppet manifest to fix failed request

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# This manuscript fixes an issue with an Apache web server
+
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => ['/bin', '/usr/local/bin', '/usr/sbin', '/usr/bin']
+}
+
+exec { 'restart_nginx':
+  command => 'service nginx restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
+  require => Exec['fix--for-nginx']
+}
